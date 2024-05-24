6a9b10b908e8297e5e9277106e0496fe0a0a8f61
LucaVillaverde <166671737+LucaVillaverde@users.noreply.github.com>
2024-05-23 22:08:21 -0300

    ll

---
+++ Curso LecaSabe/Practica_Clase7/script.js
@@ -0,0 +1,123 @@
+let x = 7;
+let y = 2;
+let suma = x + y;
+let resta = x - y;
+
+/*
+alert("El resultado de la suma es " + suma);
+alert("El resultado de la resta es " + resta);
+alert("El resultado de la multiplicacion es " + x*y);
+alert("El resultado de la division es " + x/y);
+alert("El resultado de hacer el resto es " + x%y);
+*/
+
+/*
+console.log("El resultado de la suma es " + (x+y));
+console.log("El resultado de la resta es " + (x-y));
+console.log("El resultado de la multiplicacion es " + (x*y));
+console.log("El resultado de la division es " + (x/y));
+console.log("El resultado de hacer el resto es " + (x%y));
+*/
+
+/*
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x != y);
+*/
+
+/*
+console.log(10 == '10');
+console.log(10 === '10');
+*/
+
+/*
+console.log(x > y && y > 0);
+console.log(x > y || y > 0);
+console.log(!(x > y));
+*/
+
+/*
+let z = 10;
+
+z += 5; // z = z + 5
+console.log(z); // 15
+
+z -= 3; // z = z - 3
+console.log(z); // 12
+
+z *= 2; // z = z * 2
+console.log(z); // 24
+
+z /= 4; // z = z / 4
+console.log(z); // 6
+
+z %= 2; // z = z % 2
+console.log(z); // 0
+*/
+
+/*
+let miArray = [];
+
+let numeros = [1, 2, 3, 4, 5];
+let colores = ['rojo', 'verde', 'azul'];
+*/
+
+/*
+for (let i = 0; i < 4; i++) {
+    numero = parseInt(prompt("Ingrese un numero"));
+    miArray.push(numero)
+}
+
+console.log(miArray);
+console.log(numeros);
+console.log(colores);
+*/
+
+/*
+console.log(colores[0]);
+console.log(colores[1]);
+console.log(colores[2]);
+*/
+
+/*
+numeros[0] = 10;
+numeros[2] = 30;
+
+console.log(numeros);
+*/
+
+/*
+let frutas = ['manzana', 'banana'];
+console.log(frutas);
+
+frutas.push('naranja');
+console.log(frutas);
+
+frutas.pop();
+console.log(frutas);
+*/
+
+/*
+let verduras = ['zanahoria', 'brocoli'];
+console.log(verduras);
+
+verduras.unshift('espinaca');
+console.log(verduras);
+
+verduras.shift();
+console.log(verduras);
+*/
+
+/*
+let numeros = [1, 2, 3, 4, 5];
+console.log(numeros);
+
+numeros.splice(1, 0, 10, 15);
+console.log(numeros);
+
+numeros.splice(1, 2);
+console.log(numeros);
+*/
