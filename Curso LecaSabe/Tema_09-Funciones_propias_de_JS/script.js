6930ed5ac9a59a0dcf3c2ebe8fd5dbaf1ed506a0
LucaVillaverde <166671737+LucaVillaverde@users.noreply.github.com>
2024-06-12 14:47:54 -0300

    .

---
+++ Curso LecaSabe/Tema_09-Funciones_propias_de_JS/script.js
@@ -0,0 +1,96 @@
+/*Ejemplo 1 parseInt*/
+
+/*
+const numero = parseInt("10");
+console.log(numero); //<---- salida: 10
+*/
+
+//---------------------------------------
+
+/*Ejemplo 2 parseInt*/
+
+/*
+let usuario = prompt("Ingresa un numero");
+let numero1 = parseInt(usuario);
+
+console.log(numero1);
+
+let numeroBinario = parseInt("1010", 2);
+console.log(numeroBinario);
+
+let numeroHex = parseInt("1F", 16);
+console.log(numeroHex);
+*/
+
+//----------------------------------------
+
+/*Ejemplo 2 parseFloat*/
+
+/*
+let flotante = parseFloat("10.56");
+console.log(flotante);
+
+const numeroFlotanteConEspacios = parseFloat(" 10.5 ");
+console.log(numeroFlotanteConEspacios);
+
+console.log(isNaN("hola"));
+console.log(isNaN(123));
+
+let edad = prompt("Ingresa tu edad");
+
+if (isNaN(edad)){
+    console.log("Debe ingresar la edad en un numero");
+}else {
+    console.log("Numero ingresado correctamente");
+    if(edad<0 || edad>100){
+        console.log("Pero debe ser mayor que 0 y menor que 100");
+    }else{
+        console.log("Todo ok");
+    }
+}
+*/
+
+//-----------------------------
+
+// Ejemplo 1 isNaN
+
+
+
+//-----------------------------
+
+// Ejemplo 2 isNaN
+
+
+//-----------------------------
+
+// Ejemplo 3 isNaN
+
+
+
+//-----------------------------
+
+// Number
+
+let  edad = Number(prompt("Ingresa tu edad"));
+alert(edad);
+
+
+//------------------------------
+
+
+// Ejemplo 1 Number
+
+
+
+
+//------------------------------
+
+// Ejemplo 2 Number
+
+
+//------------------------------
+
+// Ejemplo 3 Number
+
+
+//------------------------------
---
+++ Curso LecaSabe/Tema_10-Eventos/script.js
@@ -0,0 +1,38 @@
+/*const boton = document.getElementById("miBoton");
+boton.addEventListener("click", function() {
+    alert("Se hizo click en el boton!");
+});
+
+const caja = document.getElementById("caja");
+//const body = document.getElementById("body");
+caja.addEventListener("mouseover", function(){
+    caja.style.backgroundColor = "red" ;
+    alert("CUIDADO LA CAJA!!!!!!");
+});
+
+caja.addEventListener("mouseout", function(){
+    caja.style.backgroundColor = "white";
+    alert("A la proxima tene mas cuidado");
+})
+
+document.addEventListener("keydown", function(event){
+    console.log("Tecla presionada: " + event.key);
+});
+*/
+
+const mod1 = document.getElementById("mod1");
+const mod2 = document.getElementById("mod2");
+let texto = document.getElementsByClassName("textoOculto")
+
+function header(){
+    var boton = document.getElementsByClassName("botonFase1")
+    if(boton.className === "botonFase1"){
+        boton.className = "oculto";
+    }else{
+        boton.className = "botonFase1";
+    }
+}
+
+mod1.addEventListener("click", header()
+    }
+)
