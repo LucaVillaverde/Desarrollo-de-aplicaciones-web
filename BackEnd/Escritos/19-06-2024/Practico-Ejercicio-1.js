2743296d43b14e3dfc596ce522e226e4d68cbd4c
LucaVillaverde <166671737+LucaVillaverde@users.noreply.github.com>
2024-07-04 20:58:19 -0300

    d

---
+++ BackEnd/Escritos/19-06-2024/Practico-Ejercicio-1.js
@@ -0,0 +1,14 @@
+// Ejercicio 1 Practico
+
+let resul = 0;
+
+for (let i = 0; i < 3; i++){
+    let input = parseInt(prompt("Ingrese un numero"));
+    if (resul = 0){
+        resul = input;
+    }
+    if (input > resul){
+        resul = input;
+    }
+}
+alert("El numero mas grande es 2" + resul);
