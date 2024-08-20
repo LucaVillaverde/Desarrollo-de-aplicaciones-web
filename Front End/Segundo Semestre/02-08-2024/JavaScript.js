bb1b1cf0a307d192eefc2944fb2d2c2481c5ac08
LucaVillaverde <166671737+LucaVillaverde@users.noreply.github.com>
2024-08-20 11:12:38 -0300

    Cambios simples

--- Front End/Segundo Semestre/02-08-2024/JavaScript.js
+++ Front End/Segundo Semestre/02-08-2024/JavaScript.js
@@ -1,19 +1,56 @@
-var menuHamburguesa = document.getElementById("menu");
-var linksNavBar = document.getElementById("myLinksAnchor");
+// var menuHamburguesa = document.getElementById("menu");
+// var linksNavBar = document.getElementById("myLinksAnchor");
 
-function interaccionNavBar(){
-    if (menuHamburguesa.className === "hamburger-menu"){
-        menuHamburguesa.className += " rotateHamb";
+// function interaccionNavBar(){
+//     if (menuHamburguesa.className === "hamburger-menu"){
+//         menuHamburguesa.className += " rotateHamb";
+//     } else {
+//         menuHamburguesa.className = "hamburger-menu";
+//     }
+//     if (linksNavBar.className == "displayAncNone"){
+//         linksNavBar.className = "displayAncBlock";
+//     } else {
+//         linksNavBar.className = "displayAncNone"
+//     }
+// }
+
+// menuHamburguesa.addEventListener("click", function() {
+//     interaccionNavBar();
+// });
+
+// function desplegar(){
+//     let height = Porcentaje();
+//     if(height === 0){
+//         document.getElementById("navigation").style.height = "100%";
+//     } else if (height === 100){
+//         document.getElementById("navigation").style.height = "0%";
+//     }
+// }
+
+// // Funcion para obtener el "porcentaje" del elemento OVERLAY
+// function Porcentaje() {
+//     // Obtener tamaño
+//     const height = document.getElementById("navigation").offsetHeight;
+//     // Cont padre
+//     const containerHeight = document.body.offsetHeight;
+//     // calcular porcentaje
+//     const overPercentage = (height /containerHeight) * 100;
+//     return overPercentage;
+// }
+
+const buttonMenu = document.getElementById("navigationButtonContainer");
+const menuNav = document.getElementById("navigation");
+
+function menu(){
+    if (menuNav.className === "default"){
+        menuNav.className += " show";
+        buttonMenu.className = "navigationButton1";
     } else {
-        menuHamburguesa.className = "hamburger-menu";
-    }
-    if (linksNavBar.className == "displayAncNone"){
-        linksNavBar.className = "displayAncBlock";
-    } else {
-        linksNavBar.className = "displayAncNone"
+        menuNav.className = "default";
+        buttonMenu.className = "none";
     }
 }
 
-menuHamburguesa.addEventListener("click", function() {
-    interaccionNavBar();
-});
+buttonMenu.addEventListener("click", function(){
+    menu();
+})
