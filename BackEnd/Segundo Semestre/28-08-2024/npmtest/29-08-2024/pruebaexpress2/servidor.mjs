bfef313b602cf03439564c35fb722987557cbf2c
LucaVillaverde <lucavilla2017@hotmail.com>
2024-08-29 17:20:06 -0300

    xd

--- BackEnd/Segundo Semestre/28-08-2024/npmtest/29-08-2024/pruebaexpress2/servidor.mjs
+++ BackEnd/Segundo Semestre/28-08-2024/npmtest/29-08-2024/pruebaexpress2/servidor.mjs
@@ -1,11 +1,14 @@
 import express from "express";
+import path from "path";
+
+const ruta = import.meta.dirname;
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hola mundo");
+    res.sendFile(path.join(ruta, "/index.html"));
 });
 
 app.listen(3000, ()=>{
-    console.log("Servidor en http://localhost:3000");
-})
+    console.log("Iniciando servidor en http://localhost:3000");
+});
